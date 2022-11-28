5d0218a09980c5b2d4b7ffdede26f7563907779f
RedPepper <pavel_ryabovol@mail.ru>
2022-11-28 16:33:15 +0500

    add font, new menu implementation

--- app/src/main/java/com/redpepper/shikiapp/ui/searchAnime/SearchAnimeFragment.kt
+++ app/src/main/java/com/redpepper/shikiapp/ui/searchAnime/SearchAnimeFragment.kt
@@ -1,19 +1,22 @@
 package com.redpepper.shikiapp.ui.searchAnime
 
 import android.os.Bundle
+import android.view.*
+import android.widget.ImageButton
+import android.widget.Toast
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.lifecycleScope
+import androidx.paging.PagingData
 import androidx.recyclerview.widget.GridLayoutManager
 import com.redpepper.shikiapp.R
 import com.redpepper.shikiapp.databinding.FragmentSearchAnimeBinding
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
+
 
 @AndroidEntryPoint
 class SearchAnimeFragment : Fragment() {
@@ -29,39 +32,69 @@ class SearchAnimeFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        }
+
+    }
 
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
+        binding = FragmentSearchAnimeBinding.inflate(inflater, container, false)
 
-        binding = FragmentSearchAnimeBinding.inflate(inflater,container, false)
+        val menuHost: MenuHost = binding.searchAnimeToolbar
+        menuHost.addMenuProvider(object : MenuProvider {
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                menuInflater.inflate(R.menu.menu, menu)
+                val menuFilterButton = menu.findItem(R.id.action_filter)
+            }
+
+            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                if(menuItem.itemId == R.id.action_filter) {
+                    Toast.makeText(context, "coming soon", Toast.LENGTH_LONG).show()
+                }
+
+                return true
+            }
+        }, viewLifecycleOwner)
+
+
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        setReryclerView()
-        searchAnime()
-    }
+        setRecyclerView()
 
-    private fun searchAnime(){
-        searchJob?.cancel()
-        searchJob = lifecycleScope.launch{
-            viewModel.getSortedByRankAnime()
-                viewModel.searchResult.observe(viewLifecycleOwner){
-                searchAnimePagingAdapter.submitData(lifecycle,it)
-            }
+        searchAnime()
+
+        binding.swipeRefresh.setOnRefreshListener {
+            searchAnime()
         }
     }
 
-    private fun  setReryclerView(){
+
+    private fun searchAnime() {
+        searchJob?.cancel()
+        searchJob = lifecycleScope.launch {
+            binding.swipeRefresh.isRefreshing = true
+
+            searchAnimePagingAdapter.submitData(PagingData.empty())
+
+            viewModel.getSortedByRankAnime()
+            viewModel.searchResult.observe(viewLifecycleOwner) {
+                searchAnimePagingAdapter.submitData(lifecycle, it)
+            }
+
+            binding.swipeRefresh.isRefreshing = false
+        }
+    }
+
+    private fun setRecyclerView() {
         val columnsCount = resources.getInteger(R.integer.itemsInRecyclerCount)
 
         binding.searchAnimeRecycler.apply {
             adapter = searchAnimePagingAdapter
-            layoutManager = GridLayoutManager(requireContext(),columnsCount)
+            layoutManager = GridLayoutManager(requireContext(), columnsCount)
         }
     }
 }
